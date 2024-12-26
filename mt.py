6378470262d564c6642eb99287d2ebcd1f10e0fb
Kick <kickwinner75@gmail.com>
2024-12-26 21:15:06 +0500

    Add files via upload

---
+++ mt.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+Facebook mobile contact page URL
+url = "(link unavailable)"
+
+Send HTTP GET request
+response = requests.get(url)
+
+Parse HTML response using BeautifulSoup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+Find specific elements on the page
+For example, let's find all contact names
+contact_names = soup.find_all('div', {'class': '_39g5'})
+
+Print contact names
+for name in contact_names:
+    print(name.text.strip())
